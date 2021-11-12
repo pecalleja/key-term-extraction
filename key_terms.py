a5382fd4b942af67c5348b61a6699a386c115b01
Pedro Calleja <pcalleja@merama.io>
2021-11-11 22:29:29 -0500

    2/4

--- key_terms.py
+++ key_terms.py
@@ -1,16 +1,28 @@
 from lxml import etree
 import nltk
 from collections import Counter
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
 
-xml_path = "news.xml"
-corpus = etree.parse(xml_path).getroot()[0]
-for new_story in corpus:
-    head, story = new_story
-    text = story.text
-    tokens = nltk.tokenize.word_tokenize(text.lower())
-    tokens.sort(reverse=True)
-    freq_list = Counter(tokens).most_common(5)
-    print(f"{head.text}:")
-    freq_words = [x[0] for x in freq_list]
-    print(" ".join(freq_words))
-    print()
+
+def main():
+    xml_path = "news.xml"
+    corpus = etree.parse(xml_path).getroot()[0]
+    wnl = WordNetLemmatizer()
+    ignore = list(string.punctuation) + stopwords.words('english')
+    for new_story in corpus:
+        head, story = new_story
+        text = story.text
+        tokens = nltk.tokenize.word_tokenize(text.lower())
+        tokens.sort(reverse=True)
+        norm = [wnl.lemmatize(term) for term in tokens]
+        norm_filtered = [x for x in norm if x not in ignore]
+        freq_list = Counter(norm_filtered).most_common(5)
+        print(head.text, end=":\n")
+        freq_words = [x[0] for x in freq_list]
+        print(*freq_words, sep=' ', end="\n\n")
+
+
+if __name__ == '__main__':
+    main()
